72274ac108c362209127534ebb91d2aa83fccf01
Dato Lomiashvili <lomiashvili.dato5@gmail.com>
2023-12-01 23:31:08 +0400

    delete scripts

--- src/Contract/HasTree.php
+++ src/Contract/HasTree.php
@@ -13,7 +13,6 @@ interface HasTree
     public function updateTree(?array $list = null): array;
     public function getTreeRecordTitle(?Model $record = null): string;
     public function getTranslatedTreeRecordTitles(?Model $record = null): array;
-
     public function getRecordKey(?Model $record): ?string;
     public function makeFilamentTranslatableContentDriver(): ?TranslatableContentDriver;
 }
